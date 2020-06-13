9376cc2a39c6f6f59e0e9f7708e3d1f1c846031d
Chengsh <ousiqiuseqian@gmail.com>
2020-06-13 12:09:33 +0800

    commit by Cheng

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Test for Github!!" << std::endl;
     std::cout << "Test for Github!! by alpaca0072" << std::endl;
+    std::cout << "Test by CHeng"<< std::endl;
     return 0;
 }
