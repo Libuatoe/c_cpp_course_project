be93165c928a7b8dfed1f8f5f328fa33e97d932f
AlpAcA0072 <AlpAcA0072@gmail.com>
2020-06-14 22:17:26 +0800

    更新了几个基本方法

--- Matrix.hpp
+++ Matrix.hpp
@@ -8,6 +8,7 @@ namespace matrix {
     public:
         T data[width][length];
 
+        //矩阵加法
         Matrix operator+(Matrix b) {
             if (this->matrixLength == b.matrixLength && this->matrixWidth == b.matrixWidth) {
                 Matrix<T, width, length> result;
@@ -20,6 +21,54 @@ namespace matrix {
             } else {
 
             }
+        }
+
+        //矩阵减法
+        Matrix operator-(Matrix b) {
+            if (this->matrixLength == b.matrixLength && this->matrixWidth == b.matrixWidth) {
+                Matrix<T, width, length> result;
+                for (int i = 0; i < width; ++i) {
+                    for (int j = 0; j < length; ++j) {
+                        result.data[i][j] = this->data[i][j] - b.data[i][j];
+                    }
+                }
+                return result;
+            } else {
+
+            }
+        }
+
+        __unused //找矩阵最大值
+        T findMax() {
+            T max = this->data[0][0];
+            for (int i = 0; i < width; ++i) {
+                for (int j = 0; j < length; ++j) {
+                    max = max > this->data[i][j] ? max : this->data[i][j];
+                }
+            }
+            return max;
+        }
+
+        __unused //找矩阵最小值
+        T findMin() {
+            T min = this->data[0][0];
+            for (int i = 0; i < width; ++i) {
+                for (int j = 0; j < length; ++j) {
+                    min = min < this->data[i][j] ? min : this->data[i][j];
+                }
+            }
+            return min;
+        }
+
+        __unused //求和
+        T sum() {
+            T sum;
+            for (int i = 0; i < width; ++i) {
+                for (int j = 0; j < length; ++j) {
+                    sum += this->data[i][j];
+                }
+            }
+            return sum;
         }
     };
 }
