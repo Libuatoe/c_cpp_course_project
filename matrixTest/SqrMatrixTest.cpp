45c2e0f364c4d84b5651c55b008471147faaf861
Chengsh <ousiqiuseqian@gmail.com>
2020-06-17 16:16:37 +0800

    对大部分运算方法写了测试代码 矩阵对复数的支持非常好 已经测试过复数了

--- matrixTest/SqrMatrixTest.cpp
+++ matrixTest/SqrMatrixTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "../matrixLibrary/SqrMatrix.hpp"
+
 using namespace matrix;
 using namespace std;
 
@@ -11,33 +12,47 @@ int main() {
                        1, 0, 5, 0};
     SqrMatrix<double> sqrMatrix1(4, array1);
 
+
     std::cout << "矩阵sqrMatrix1的大小为:" << sqrMatrix1.getRow() << endl;
     std::cout << "矩阵sqrMatrix1的内容为:" << endl;
     sqrMatrix1.showMatrix();
+
+    std::cout << "矩阵qrMatrix1的转置矩阵内容为:" << endl;
+    sqrMatrix1.transpose().showMatrix();
+
     std::cout << "矩阵sqrMatrix1的trace为:" << sqrMatrix1.trace() << std::endl;
     std::cout << "矩阵sqrMatrix1的行列式为:" << sqrMatrix1.determinant() << std::endl;
     std::cout << "矩阵sqrMatrix1的逆矩阵为:" << std::endl;
     SqrMatrix<double> sqrMatrix2(4);
     sqrMatrix2 = sqrMatrix1.inverse();
     sqrMatrix2.showMatrix();
-    Matrix<double> sqrMatrix3(4,4);
+    Matrix<double> sqrMatrix3(4, 4);
 
-//    std::cout <<"设定一个新的4 * 4方阵 sqrMatrix3"<<endl;
-//
-//    double array2[] = {
-//            1,3,7,8,
-//            2,5,9,0,
-//            4,6,7,9,
-//            3,5,8,1
-//    };
-//    SqrMatrix<double> sqrMatrix3(4, array2);
-//    std::cout << "矩阵sqrMatrix3的内容为:" << endl;
-//    sqrMatrix3.showMatrix();
-//
-//    std::cout<< "将矩阵strMatrix3 和 strMatrix1 相乘后得到新矩阵为:"<<endl;
-//    Matrix<double> matrix1 = (Matrix<double>)sqrMatrix3;
-//    Matrix<double> matrix2 = (Matrix<double>) sqrMatrix1;
-//    Matrix<double> matrix3 = matrix1 * matrix3;
-//    matrix3.showMatrix();
+    std::cout << "设定一个新的4 * 4方阵 sqrMatrix4" << endl;
+
+    double array2[] = {
+            1, 3, 7, 8,
+            2, 5, 9, 0,
+            4, 6, 7, 9,
+            3, 5, 8, 1
+    };
+    Matrix<double> sqrMatrix4(4, 4, array2);
+    Matrix<double> sqrMatrix5(4, 4);
+
+    std::cout << "矩阵sqrMatrix4的内容为:" << endl;
+    sqrMatrix4.showMatrix();
+
+    std::cout << "将矩阵sqrMatrix4 和 sqrMatrix1 相乘后得到新矩阵为:" << endl;
+    sqrMatrix5 = sqrMatrix4 * sqrMatrix1;
+    sqrMatrix5.showMatrix();
+
+    std::cout << "将矩阵sqrMatrix4 和 sqrMatrix1 相加后得到新矩阵为:" << endl;
+    sqrMatrix5 = sqrMatrix4 + sqrMatrix1;
+    sqrMatrix5.showMatrix();
+
+    std::cout << "将矩阵sqrMatrix4 和 sqrMatrix1 相减后得到新矩阵为:" << endl;
+    sqrMatrix5 = sqrMatrix4 - sqrMatrix1;
+    sqrMatrix5.showMatrix();
+
 
 }
